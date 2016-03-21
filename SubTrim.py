21c925f586e019db415ea4f7f697a23117f1170d
Andreas Naess <andreas.n92@gmail.com>
2016-03-21 12:24:02 +0100

    Builds new srt file without hearing impairment

--- SubTrim.py
+++ SubTrim.py
@@ -2,26 +2,59 @@ import re
 import sys
 
 lines = []
+indexCount = 0
+index = []
+timestamp = []
+subtitle = []
+
 try:
-    input = open(sys.argv[1], 'r')
-    lines = input.readlines()
-    input.close()
+    f = open(sys.argv[1], 'r')
+    data = f.read()
+    paragraphList = data.split("\n\n")
+    count = 0
+    for paragraph in paragraphList:
+        paragraphLines = paragraph.split("\n")
+        if not paragraphLines[0]:
+            continue
+        count += 1
+        indexCount += 1
+        index.append(str(indexCount))
+        timestamp.append(paragraphLines[1])
+        tempSub = ""
+        parenthesesSeen = False
+        for line in paragraphLines[2:]:
+            if re.search('\(|\)', line):
+                parenthesesSeen = True
+                continue
+            subjectMatch = re.search('^[A-Z]*:', line)
+            if subjectMatch:
+                substring = subjectMatch.group(0) + " "
+                newLine = line.replace(substring, "")
+                tempSub += newLine + '\n'
+                continue
+            tempSub += line + '\n'
+
+        if not tempSub:
+            if parenthesesSeen:
+                index.pop()
+                indexCount -= 1
+                timestamp.pop()
+                continue
+
+        if tempSub:
+            subtitle.append(tempSub)
 
     output = open(sys.argv[2], 'w')
-    for line in lines:
-        # \((.|\n)*\)
-        if re.search('\(|\)', line):
-            continue
-        subjectMatch = re.search('^[A-Z]*:', line)
-        if subjectMatch:
-            substring = subjectMatch.group(0) + " "
-            newLine = line.replace(substring, "")
-            output.write(newLine)
-            continue
-        output.write(line)
+    for x in range(len(index)):
+        output.write(index[x] + '\n')
+        output.write(timestamp[x] + '\n')
+        output.write(subtitle[x] + '\n')
+
     output.close()
     print 'Subtitle trim complete!'
-except IndexError:
+except IndexError, e:
+    # print e
     print 'Set an input and output file argument'
-except IOError:
+except IOError, e:
+    # print e
     print 'No such file exists!'
